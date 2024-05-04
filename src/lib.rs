5fd1b4acf48974c0680e33a9fc4a8f4a55541fad
Dan Bluhm Hansen <00.pavers_dither@icloud.com>
2024-05-04 17:01:03 +0200

    init rework

---
+++ src/lib.rs
@@ -0,0 +1,259 @@
+use pgrx::{pg_sys::panic::ErrorReportable, prelude::*, spi::SpiError};
+
+::pgrx::pg_module_magic!();
+
+#[pg_extern]
+fn json_diff(left: pgrx::Json, right: pgrx::Json) -> serde_json::Result<pgrx::Json> {
+    Ok(pgrx::Json(serde_json::to_value(json_patch::diff(
+        &left.0, &right.0,
+    ))?))
+}
+
+#[pg_extern]
+fn jsonb_diff(left: pgrx::JsonB, right: pgrx::JsonB) -> serde_json::Result<pgrx::JsonB> {
+    Ok(pgrx::JsonB(serde_json::to_value(json_patch::diff(
+        &left.0, &right.0,
+    ))?))
+}
+
+struct JsonPatch;
+
+#[pg_aggregate]
+impl Aggregate for JsonPatch {
+    const NAME: &'static str = "json_patch";
+    const INITIAL_CONDITION: Option<&'static str> = Some("{}");
+
+    type State = pgrx::Json;
+    type Args = pgrx::name!(value, pgrx::Json);
+
+    #[pgrx(parallel_safe, immutable, strict)]
+    fn state(
+        mut current: Self::State,
+        arg: Self::Args,
+        _fcinfo: pg_sys::FunctionCallInfo,
+    ) -> Self::State {
+        // TODO: avoid clone?
+        if let Ok(patch) = serde_json::from_value::<json_patch::Patch>(arg.0.clone()) {
+            json_patch::patch(&mut current.0, &patch).unwrap_or_report();
+        } else {
+            json_patch::merge(&mut current.0, &arg.0);
+        }
+        current
+    }
+}
+
+struct JsonBPatch;
+
+#[pg_aggregate]
+impl Aggregate for JsonBPatch {
+    const NAME: &'static str = "jsonb_patch";
+    const INITIAL_CONDITION: Option<&'static str> = Some("{}");
+
+    type State = pgrx::JsonB;
+    type Args = pgrx::name!(value, pgrx::JsonB);
+
+    #[pgrx(parallel_safe, immutable, strict)]
+    fn state(
+        mut current: Self::State,
+        arg: Self::Args,
+        _fcinfo: pg_sys::FunctionCallInfo,
+    ) -> Self::State {
+        // TODO: avoid clone?
+        if let Ok(patch) = serde_json::from_value::<json_patch::Patch>(arg.0.clone()) {
+            json_patch::patch(&mut current.0, &patch).unwrap_or_report();
+        } else {
+            json_patch::merge(&mut current.0, &arg.0);
+        }
+        current
+    }
+}
+
+struct JsonMerge;
+
+#[pg_aggregate]
+impl Aggregate for JsonMerge {
+    const NAME: &'static str = "json_merge";
+    const INITIAL_CONDITION: Option<&'static str> = Some("{}");
+
+    type State = pgrx::Json;
+    type Args = pgrx::name!(value, pgrx::Json);
+
+    #[pgrx(parallel_safe, immutable, strict)]
+    fn state(
+        mut current: Self::State,
+        arg: Self::Args,
+        _fcinfo: pg_sys::FunctionCallInfo,
+    ) -> Self::State {
+        json_patch::merge(&mut current.0, &arg.0);
+        current
+    }
+}
+
+struct JsonBMerge;
+
+#[pg_aggregate]
+impl Aggregate for JsonBMerge {
+    const NAME: &'static str = "jsonb_merge";
+    const INITIAL_CONDITION: Option<&'static str> = Some("{}");
+
+    type State = pgrx::JsonB;
+    type Args = pgrx::name!(value, pgrx::JsonB);
+
+    #[pgrx(parallel_safe, immutable, strict)]
+    fn state(
+        mut current: Self::State,
+        arg: Self::Args,
+        _fcinfo: pg_sys::FunctionCallInfo,
+    ) -> Self::State {
+        json_patch::merge(&mut current.0, &arg.0);
+        current
+    }
+}
+
+struct First;
+
+#[pg_aggregate]
+impl Aggregate for First {
+    type State = pgrx::AnyElement;
+    type Args = pgrx::name!(value, pgrx::AnyElement);
+
+    #[pgrx(parallel_safe, immutable, strict)]
+    fn state(
+        current: Self::State,
+        _arg: Self::Args,
+        _fcinfo: pg_sys::FunctionCallInfo,
+    ) -> Self::State {
+        current
+    }
+}
+
+struct Last;
+
+#[pg_aggregate]
+impl Aggregate for Last {
+    type State = pgrx::AnyElement;
+    type Args = pgrx::name!(value, pgrx::AnyElement);
+
+    #[pgrx(parallel_safe, immutable, strict)]
+    fn state(
+        _current: Self::State,
+        arg: Self::Args,
+        _fcinfo: pg_sys::FunctionCallInfo,
+    ) -> Self::State {
+        arg
+    }
+}
+
+#[pg_extern]
+fn migrate() -> Result<(), SpiError> {
+    Spi::run("create extension if not exists pgcrypto;")?;
+    Spi::run(include_str!("../sql/init.sql"))?;
+    Spi::run(include_str!("../sql/users.sql"))?;
+
+    Ok(())
+}
+
+#[cfg(any(test, feature = "pg_test"))]
+#[pg_schema]
+mod tests {
+    use pgrx::prelude::*;
+
+    #[pg_test]
+    fn user_insert() {
+        _ = Spi::run("select migrate();");
+
+        let user_id = Spi::get_one::<pgrx::Uuid>(
+            "insert into users (username, salt, passhash) values ('foo', '', '') returning id;",
+        )
+        .expect("user initialized successfully");
+        let (stream_id, data) =
+            Spi::get_two::<pgrx::Uuid, pgrx::JsonB>("select stream_id, data from user_events;")
+                .expect("user initialized successfully");
+
+        assert_eq!(user_id, stream_id);
+        assert_eq!(
+            Some(serde_json::json!({ "username": "foo", "passhash": "", "salt": "" })),
+            data.map(|d| d.0)
+        );
+    }
+
+    #[pg_test]
+    fn user_update() {
+        _ = Spi::run("select migrate();");
+
+        _ = Spi::run("insert into users (username, salt, passhash) values ('foo', '', '');");
+        _ = Spi::run("update users set username = 'bar';");
+
+        let name = Spi::get_one::<&str>(
+            "select data ->> 'username' from user_events where name = 'init';",
+        )
+        .expect("user initialized successfully");
+        let data = Spi::get_one::<pgrx::JsonB>("select data from user_events where name = 'set';")
+            .map(|opt| opt.map(|data| data.0));
+
+        assert_eq!(Some("foo"), name);
+        assert_eq!(
+            Ok(Some(
+                serde_json::json!([{"op": "replace", "path": "/username", "value": "bar"}])
+            )),
+            data
+        );
+    }
+
+    #[pg_test]
+    fn user_update_set_null() {
+        _ = Spi::run("select migrate();");
+
+        _ = Spi::run("insert into users (username, salt, passhash, email) values ('foo', '', '', 'foo@bar.com');");
+        _ = Spi::run("update users set email = null;");
+
+        let data = Spi::get_one::<pgrx::JsonB>(
+            "select data from user_events where name = 'set' order by timestamp desc;",
+        )
+        .map(|opt| opt.map(|data| data.0));
+
+        assert_eq!(
+            Ok(Some(
+                serde_json::json!([{"op": "remove", "path": "/email"}])
+            )),
+            data
+        );
+    }
+
+    #[pg_test]
+    fn user_delete() {
+        _ = Spi::run("select migrate();");
+
+        _ = Spi::run("insert into users (username, salt, passhash) values ('foo', '', '');");
+        _ = Spi::run("delete from users;");
+
+        let name = Spi::get_one::<&str>(
+            "select data ->> 'username' from user_events where name = 'init';",
+        )
+        .expect("user initialized successfully");
+        let data = Spi::get_one::<pgrx::JsonB>("select data from user_events where name = 'drop';")
+            .map(|opt| opt.map(|data| data.0));
+
+        assert_eq!(Some("foo"), name);
+        assert_eq!(
+            Ok(Some(
+                serde_json::json!([{"op": "replace", "path": "", "value": null}])
+            )),
+            data
+        );
+    }
+}
+
+/// This module is required by `cargo pgrx test` invocations.
+/// It must be visible at the root of your extension crate.
+#[cfg(test)]
+pub mod pg_test {
+    pub fn setup(_options: Vec<&str>) {
+        // perform one-off initialization when the pg_test framework starts
+    }
+
+    pub fn postgresql_conf_options() -> Vec<&'static str> {
+        // return any postgresql.conf settings that are required for your tests
+        vec![]
+    }
+}
