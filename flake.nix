6e912016378d07568e7e825af0c14e3f0016b902
Dan Bluhm Hansen <00.pavers_dither@icloud.com>
2024-09-08 12:42:41 +0200

    nix flake

---
+++ flake.nix
@@ -0,0 +1,148 @@
+{
+  description = "Build a cargo workspace";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane.url = "github:ipetkov/crane";
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
+    fenix.inputs.rust-analyzer-src.follows = "";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    crane,
+    fenix,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      inherit (pkgs) lib;
+
+      craneLib = crane.mkLib pkgs;
+      src = craneLib.cleanCargoSource ./.;
+
+      # Common arguments can be set here to avoid repeating them later
+      commonArgs = {
+        inherit src;
+        strictDeps = true;
+
+        buildInputs = with pkgs;
+          [
+            # Add additional build inputs here
+            pkg-config
+            readline
+            zlib
+            openssl
+          ]
+          ++ lib.optionals stdenv.isDarwin [
+            # Additional darwin specific inputs can be set here
+            libiconv
+            darwin.apple_sdk.frameworks.CoreFoundation
+            darwin.apple_sdk.frameworks.CoreServices
+            darwin.apple_sdk.frameworks.Security
+            darwin.apple_sdk.frameworks.SystemConfiguration
+          ];
+
+        # Additional environment variables can be set directly
+        # MY_CUSTOM_VAR = "some value";
+      };
+
+      craneLibLLvmTools =
+        craneLib.overrideToolchain
+        (fenix.packages.${system}.complete.withComponents [
+          "cargo"
+          "llvm-tools"
+          "rustc"
+        ]);
+
+      # Build *just* the cargo dependencies (of the entire workspace),
+      # so we can reuse all of that work (e.g. via cachix) when running in CI
+      # It is *highly* recommended to use something like cargo-hakari to avoid
+      # cache misses when building individual top-level-crates
+      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+      individualCrateArgs =
+        commonArgs
+        // {
+          inherit cargoArtifacts;
+          inherit (craneLib.crateNameFromCargoToml {inherit src;}) version;
+          # NB: we disable tests since we'll run them all via cargo-nextest
+          doCheck = false;
+        };
+
+      fileSetForCrate = crate:
+        lib.fileset.toSource {
+          root = ./.;
+          fileset = lib.fileset.unions [
+            ./Cargo.toml
+            ./Cargo.lock
+            crate
+          ];
+        };
+
+      # Build the top-level crates of the workspace as individual derivations.
+      # This allows consumers to only depend on (and build) only what they need.
+      # Though it is possible to build the entire workspace as a single derivation,
+      # so this is left up to you on how to organize things
+      tankard_db = craneLib.buildPackage (individualCrateArgs
+        // {
+          pname = "tankard_db";
+          cargoExtraArgs = "-p db";
+          src = fileSetForCrate ./db;
+        });
+      tankard_srv = craneLib.buildPackage (individualCrateArgs
+        // {
+          pname = "tankard_srv";
+          cargoExtraArgs = "-p srv";
+          src = fileSetForCrate ./srv;
+        });
+    in {
+      checks = {
+        # Build the crates as part of `nix flake check` for convenience
+        inherit tankard_db tankard_srv;
+      };
+
+      packages =
+        {
+          inherit tankard_db tankard_srv;
+        }
+        // lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
+          ws-llvm-coverage = craneLibLLvmTools.cargoLlvmCov (commonArgs
+            // {
+              inherit cargoArtifacts;
+            });
+        };
+
+      apps = {
+        db = flake-utils.lib.mkApp {
+          drv = tankard_db;
+        };
+        srv = flake-utils.lib.mkApp {
+          drv = tankard_srv;
+        };
+      };
+
+      devShells.default = craneLib.devShell {
+        # Inherit inputs from checks.
+        checks = self.checks.${system};
+
+        # Additional dev-shell environment variables can be set directly
+        DATABASE_URL = "postgres://localhost:28816/tankard";
+
+        # Extra inputs can be added here; cargo and rustc are provided by default.
+        packages = with pkgs; [
+          cargo-outdated # outdated crates
+          alejandra # nix formatter
+          deno # formatters
+          nil # nix language server
+          rust-analyzer # rust language server
+          usql # sql cli
+        ];
+      };
+    });
+}
